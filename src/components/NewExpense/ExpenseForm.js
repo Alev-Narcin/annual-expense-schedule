4c18013b3b73bb940cb8ff17ffb8d73455729738
alev.narcin <alev.narcin@mavinci.com.tr>
2022-06-27 10:05:46 +0300

    handling form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -33,7 +33,8 @@ const ExpenseForm = () => {
         /*setUserInput({
             ...userInput,
             enteredAmount: event.target.value,
-        })*/;
+        })*/
+        ;
     };
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
@@ -44,8 +45,19 @@ const ExpenseForm = () => {
         });*/
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
