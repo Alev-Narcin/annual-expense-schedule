ce7ace611a88045061d805c2ebb6dcbada9496a6
Alev <alev.narcin@mavinci.com.tr>
2022-06-28 22:54:15 +0300

    Wrap Up & Next Steps

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -5,7 +5,6 @@ const ExpensesFilter = (props) => {
 
     const dropDownChangeHandler = (event) => {
         props.onChangeFilter(event.target.value);
-        console.log(event.target.value);
     };
 
     return (
